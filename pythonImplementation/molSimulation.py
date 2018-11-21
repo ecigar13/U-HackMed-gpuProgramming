176febddc19ed1f536bce34bd4bbe96d06b7409e
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-11-21 17:49:15 -0500

    Added simulation of movement. Need to do association. Got segmentation fault for now while doing cProfile but not in regular run.

--- pythonImplementation/molSimulation.py
+++ pythonImplementation/molSimulation.py
@@ -1,9 +1,14 @@
 import numpy as np
-import scipy
-from numba import jit, vectorize, guvectorize, float64, complex64, int64
+from numba import jit, vectorize, guvectorize, float64, complex64, int64, prange
 
 import random, cProfile
 
+@jit
+def move3d(seed, x, y, z, deviation):
+    """Return a pair of new x,y coordinate for a given input x, y based on the seed. """
+    random.seed(seed)
+    return random.randrange(x - deviation, x + deviation), random.randrange(y - deviation, y + deviation), \
+           random.randrange(z - deviation, z + deviation)
 
 @jit
 def trueFalse(seed):
@@ -13,35 +18,51 @@ def trueFalse(seed):
 
 
 @jit
-def move2d(seed, x, y, deviation):
+def move2d(seed, xy, deviation):
     """Return a pair of new x,y coordinate for a given input x, y based on the seed. """
     random.seed(seed)
-    return random.randrange(x - deviation, x + deviation), random.randrange(y - deviation, y + deviation)
+    return random.randrange(xy[0] - deviation, xy[0] + deviation), random.randrange(xy[1] - deviation, xy[1] +
+                                                                                    deviation)
 
 
 @jit
-def move3d(seed, x, y, z, deviation):
-    """Return a pair of new x,y coordinate for a given input x, y based on the seed. """
-    random.seed(seed)
-    return random.randrange(x - deviation, x + deviation), random.randrange(y - deviation, y + deviation), \
-           random.randrange(z - deviation, z + deviation)
+def moveAllParticles2d(seed, deviation, pointArray):
+    for i in prange(pointArray.size):
+        #print(pointArray[i])
+        pointArray[i] = move2d(seed,pointArray[i], deviation)
+        #print(pointArray[i])
+    return pointArray
+
 
 
 @jit
 def initiateParticles2d(numberOfParticle, bound):
     """Return a 2d array of x and y within a certain bound. The index stands for the id of particle."""
-    return np.random.randint(bound, size=(numberOfParticle,2))
+    return np.random.randint(bound, size=(numberOfParticle, 2))
+
 
 @jit
-def initiateParticles2d(numberOfParticle, bound):
+def initiateParticles3d(numberOfParticle, bound):
     """Return a 2d array of x, y, z within a certain bound. The index stands for the id of particle."""
-    return np.random.randint(bound, size=(numberOfParticle,3))
+    return np.random.randint(bound, size=(numberOfParticle, 3))
 
 
-cProfile.run("initiateParticles2d(9999,9)")
 ##initiate a bunch of particles
 ##set the bounds
 ##move them ---> will use a lot
 ##associate them ---> if they do so.
 ##move the pairs
 ##disassociate the pairs.
+
+
+##Associate particles
+
+@jit
+def main(numberOfParticle, bound, seed, deviation, iterations):
+    molArray = initiateParticles2d(numberOfParticle,bound)
+    #print(molArray)
+    for i in prange(iterations):
+        moveAllParticles2d(seed, deviation, molArray)
+        #print(molArray)
+
+cProfile.run("main(99999,99,1,2,10)")
