18e6e9302e16fc9337533ef640942c09c4d4bd00
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-11-23 00:02:19 -0500

    Fixed segmentation fault and added parallel calculation of movement.

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # U-HackMed-gpuProgramming
 
-Hello Everyone,
+The bottleneck of moving data to GPU and back means I should rely on CPU in certain areas and GPU in certain areas, not always GPU. So far I am using Numba and Numpy.
 
-You can find some useful GPU programming resources below:
+A wild idea is to divide the space into squares and calculate association based on what's in the squares. Then implement parallelization on those squares.
+
+Next step: learn about Delaunay triangulation, Bowyer-Watson algo and how they work. I don't know anything now. Useful links:
+ - Delaunay: https://en.wikibooks.org/wiki/Trigonometry/For_Enthusiasts/Delaunay_triangulation
+ - Delaunay: https://en.wikipedia.org/wiki/Delaunay_triangulation
+ - Bowyer-Watson: https://en.wikipedia.org/wiki/Bowyer%E2%80%93Watson_algorithm
+
 
 Very useful websites:
  - Python list comprehension: https://www.ibm.com/developerworks/community/blogs/jfp/entry/Python_List_Comprehensions?lang=en_us
  - Speed comparison on LU factorization: https://www.ibm.com/developerworks/community/blogs/jfp/entry/A_Comparison_Of_C_Julia_Python_Numba_Cython_Scipy_and_BLAS_on_LU_Factorization?lang=en_us
  - Speed comparison on Mandelbrodt: https://www.ibm.com/developerworks/community/blogs/jfp/entry/How_To_Compute_Mandelbrodt_Set_Quickly?lang=en
- 
- - https://www.mathworks.com/company/newsletters/articles/gpu-programming-in-matlab.html
-   
- - https://www.mathworks.com/discovery/matlab-gpu.html
 
 Book:
     Programming with massively parallel processors, 3rd edition by David B. Kirk, Wen-mei W. Hwu
--- pythonImplementation/molSimulation.py
+++ pythonImplementation/molSimulation.py
@@ -1,50 +1,42 @@
 import numpy as np
-from numba import jit, vectorize, guvectorize, float64, complex64, int64, prange
+from numba import jit, vectorize, guvectorize, float64, complex64, int64, prange, njit
+import random
 
-import random, cProfile
 
-@jit
+@jit(parallel=True)
 def move3d(seed, x, y, z, deviation):
     """Return a pair of new x,y coordinate for a given input x, y based on the seed. """
     random.seed(seed)
-    return random.randrange(x - deviation, x + deviation), random.randrange(y - deviation, y + deviation), \
-           random.randrange(z - deviation, z + deviation)
+    return random.randrange(x - deviation, x + deviation), random.randrange(y - deviation,
+                                                                            y + deviation), random.randrange(
+        z - deviation, z + deviation)
 
-@jit
+
+@jit(parallel=True)
 def trueFalse(seed):
     """Return random true false based on the seed. Need to improve this probability to match Khuloud Jaquaman's matlab."""
     random.seed(seed)
     return random.getrandbits(1)
 
 
-@jit
-def move2d(seed, xy, deviation):
-    """Return a pair of new x,y coordinate for a given input x, y based on the seed. """
-    random.seed(seed)
-    return random.randrange(xy[0] - deviation, xy[0] + deviation), random.randrange(xy[1] - deviation, xy[1] +
-                                                                                    deviation)
-
-
-@jit
+@jit(parallel=True)
 def moveAllParticles2d(seed, deviation, pointArray):
-    for i in prange(pointArray.size):
-        #print(pointArray[i])
-        pointArray[i] = move2d(seed,pointArray[i], deviation)
-        #print(pointArray[i])
+    for i in range(pointArray.shape[0]):
+        for j in range(pointArray.shape[1]):
+            pointArray[i, j] += random.randrange(-deviation, deviation)
     return pointArray
 
 
-
-@jit
+@jit(parallel=True)
 def initiateParticles2d(numberOfParticle, bound):
     """Return a 2d array of x and y within a certain bound. The index stands for the id of particle."""
-    return np.random.randint(bound, size=(numberOfParticle, 2))
+    return np.random.randint(bound, size=(numberOfParticle, 2), dtype=np.int64)
 
 
-@jit
+@jit(parallel=True)
 def initiateParticles3d(numberOfParticle, bound):
     """Return a 2d array of x, y, z within a certain bound. The index stands for the id of particle."""
-    return np.random.randint(bound, size=(numberOfParticle, 3))
+    return np.random.randint(bound, size=(numberOfParticle, 3), dtype=np.int64)
 
 
 ##initiate a bunch of particles
@@ -59,10 +51,12 @@ def initiateParticles3d(numberOfParticle, bound):
 
 @jit
 def main(numberOfParticle, bound, seed, deviation, iterations):
-    molArray = initiateParticles2d(numberOfParticle,bound)
-    #print(molArray)
+    """Do not use parallel=True here. This is sequential. Simulate movement in certain iterations."""
+    molArray = initiateParticles2d(numberOfParticle, bound)
+    print(molArray)
     for i in prange(iterations):
-        moveAllParticles2d(seed, deviation, molArray)
-        #print(molArray)
+        molArray = moveAllParticles2d(seed, deviation, molArray)
+    print(molArray)
 
-cProfile.run("main(99999,99,1,2,10)")
+
+main(99999, 999, 0, 2, 20)
